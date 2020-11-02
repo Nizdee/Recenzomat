503c1999b49db320637dd83465fd2f38ad500bea
Denis Tišljar <72692064+Nizdee@users.noreply.github.com>
2020-11-02 02:14:31 +0100

    2 commit - osnovni kostur stranice

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <h2><b>Recenzomat</b></h2>
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+    <div class="container">
+    <Recenzija msg="Dobrodošli na početnu stranicu.Unesite prvu recenziju"/>
+    </div>
+    <div class="container">
+      <h2><b>Pogledajte tuđe recenzije</b></h2>
+    </div>
+    <div class="columns is-4-desktop">
+      <div class="column">
+        <img src="./assets/drive.jpg">
+        <p><b>Korisnik:</b>Ivan Ivanec</p>
+        <p><b>Ocjena:</b>9</p>
+        <p><b>Recenzija:</b>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </div>
+      <div class="column">
+        <img src="./assets/drive.jpg">
+        <p><b>Korisnik:</b>Ivan Ivanec</p>
+        <p><b>Ocjena:</b>9</p>
+        <p><b>Recenzija:</b>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+    </div>
+
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Recenzomat</strong> Student - Denis Tišljar
+        </p>
+      </div>
+    </footer>
+    
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Recenzija from './components/Recenzija.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Recenzija
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  height:100%;
+}
+div.container{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 12px;
+}
+div.column{
+  text-align: center;
+  padding: 50px;
 }
 </style>
---
+++ src/components/Recenzija.vue
@@ -0,0 +1,41 @@
+<template>
+    <form>
+        <b-field label="Ime filma">
+            <b-input></b-input>
+        </b-field>
+        <b-field horizontal label="Vaša recenzija">
+            <b-input type="textarea"></b-input>
+        </b-field>
+        <b-field label="Ocjena">
+            <b-slider size="is-medium" :min="0" :max="10">
+                <template v-for="val in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]">
+                    <b-slider-tick :value="val" :key="val">{{ val }}</b-slider-tick>
+                </template>
+            </b-slider>
+        </b-field>
+        <b-field label="Zabilježite datum gledanja">
+        <b-datepicker :focused-date="date"
+            :first-day-of-week="1"
+            placeholder="Kliknite za selekciju">
+
+        </b-datepicker>
+    </b-field>
+        <b-button rounded type="submit">Dodaj recenziju</b-button>
+    </form>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
